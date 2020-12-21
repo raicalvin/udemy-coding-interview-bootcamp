f2d5a0e54ca6e252d300b0ab57dcfd4669d4a349
Simran Rai <simranrai@Simrans-MacBook-Pro.local>
2020-12-20 18:28:44 -0600

    finished reverse int problem

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+    let isNegative = n < 0
+
+    let str = n.toString().split('')
+
+    let reversedString = ""
+
+    for (let ch of str) {
+        if (ch != "-") {
+            reversedString = ch + reversedString
+        }
+    }
+
+    let reversedNumber = parseInt(reversedString)
+
+    if (isNegative) {
+        reversedNumber = -1 * reversedNumber
+    }
+
+    return reversedNumber
+
+}
 
 module.exports = reverseInt;
