a0895a86fd593521d06c899596afadfdbe84a726
Simran Rai <simranrai@Simrans-MacBook-Pro.local>
2020-12-29 19:55:11 -0600

    finished maxchars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    // Create the map/dictionary
+    // Create an array out of the string
+    // count up the characters
+    // find the max character
+    let characters = {}
+
+    for (let ch of str) {
+        if (!characters[ch]) {
+            characters[ch] = 1
+        } else {
+            characters[ch] += 1
+        }
+    }
+
+    let maxCount = 0
+    let maxCharacter = ""
+    for (let key in characters) {
+        if (characters[key] > maxCount) {
+            maxCount = characters[key]
+            maxCharacter = key
+        }
+    }
+
+    return maxCharacter
+}
 
 module.exports = maxChar;
