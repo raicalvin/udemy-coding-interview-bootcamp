926debfbae258294bdbe30b14edbb71de65aced9
Simran Rai <simranrai@Simrans-MacBook-Pro.local>
2021-01-01 16:15:49 -0600

    finished fizzbuzz and chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+    const answer = []
+
+    let chunked = []
+    for (let i = 0; i < array.length; i += 1) {
+        chunked.push(array[i])
+        if (chunked.length == size) {
+            answer.push(chunked)
+            chunked = []
+        }
+    }
+
+    if (chunked.length > 0) {
+        answer.push(chunked)
+    }
+
+    return answer
+
+}
 
 module.exports = chunk;
