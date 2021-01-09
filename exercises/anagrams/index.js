5e9432874635a5b3e3266763ceaa97d469145b28
Simran Rai <simranrai@Simrans-MacBook-Pro.local>
2021-01-09 10:24:00 -0600

    finished anagrams and capitalize

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+     const wordA = stringA.replace(/[^\w]/g, "").toLowerCase()
+     const wordB = stringB.replace(/[^\w]/g, "").toLowerCase()
+
+    let mapA = {}
+    let mapB = {}
+
+     for (let c of wordA) {
+        mapA[c] = mapA[c] + 1 || 1
+     }
+
+     for (let c of wordB) {
+        mapB[c] = mapB[c] + 1 || 1
+     }
+
+     console.log("Comparing the two maps...")
+     console.log(mapA.size)
+     if (Object.keys(mapA).length != Object.keys(mapB).length) {
+         return false
+     } else {
+         // iterate through each key and check to see if not same, return false
+         for (const [key, value] of Object.entries(mapA)) {
+            if (mapB[key] != value) {
+                return false
+            }
+         }
+     }
+
+     return true
+}
 
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,40 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//     const splitString = str.split(" ")
+//     let answer = ""
+    
+//     for (let i = 0; i < splitString.length; i++) {
+//         const word = splitString[i]
+//         const firstLetter = word[0].toUpperCase()
+//         const restOfWord = word.slice(1)
+//         const combined = firstLetter + restOfWord
+//         if (i == splitString.length - 1) {
+//             answer += combined
+//         } else {
+//             answer += combined +  " "
+//         }
+//     }
+
+//     return answer
+// }
+
+function capitalize(str) {
+    let answer = ""
+    for (let i = 0; i < str.length; i++) {
+        let letter = str.charAt(i)
+        if (i != 0) {
+            if (str.charAt(i-1) == " ") {
+                answer += letter.toUpperCase()
+            } else {
+                answer += letter
+            }
+        } else {
+            answer += letter.toUpperCase()
+        }
+    }
+    return answer
+}
 
 module.exports = capitalize;
